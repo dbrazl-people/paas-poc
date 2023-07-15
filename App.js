2c51ee8c9f6b99bbb6e42c93db8aa8791da3b8f2
Daniel Braz <danielbraz@MacBook-Air-de-Daniel.local>
2023-07-14 22:24:50 -0300

    feat: using paas library

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "paas-library";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Running POC</Text>
+      <View style={styles.buttonWrapper}>
+        <Button />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingRight: 20,
+    paddingLeft: 20,
+  },
+  buttonWrapper: {
+    marginTop: 20,
+    width: 300,
   },
 });
